d802810c326aa3e5d41b76657b030ecfdc29ff04
Daniel Dias <dandanielddp@gmail.com>
2024-06-12 01:04:53 -0300

    VelocidadeCamera

--- Assets/Scripts/MoveTowards.cs
+++ Assets/Scripts/MoveTowards.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst;
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 
 public class MoveTowards : MonoBehaviour
@@ -7,12 +9,33 @@ public class MoveTowards : MonoBehaviour
     public float speed = 1f;
     public float offset;
     public Transform player;
+    public Vector3 dir;
+    public float minSpeed;
+    public float maxSpeed;
+    public float maxDistance;
+
 
     // Update is called once per frame
     void Update()
     {
+        dir = player.transform.position - transform.position;
+        
+
+        if (dir.magnitude > maxDistance)
+        {
+            speed = maxSpeed;
+        }
+        else
+        {
+            speed = minSpeed;
+        }
+        
+
         //transform.position  = new Vector3( player.position.x * offset, transform.position.y, transform.position.z);
         transform.Translate(0,0,speed * Time.deltaTime,Space.World);
+
+
+        
     }
 
     
